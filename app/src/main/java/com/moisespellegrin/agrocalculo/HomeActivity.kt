6600eb646e4bdf9e8eb55495a93d41a5e56ff737
MoisaAP <moisespellegrin@gmail.com>
2025-08-24 22:44:25 -0300

    Atualização Activity

--- app/src/main/java/com/moisespellegrin/agrocalculo/HomeActivity.kt
+++ app/src/main/java/com/moisespellegrin/agrocalculo/HomeActivity.kt
@@ -5,27 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.content.Intent
 
-
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
         }
     }
 }
