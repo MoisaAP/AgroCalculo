adbe717daa561550971b56159a9b517156201430
MoisaAP <moisespellegrin@gmail.com>
2025-08-25 22:50:58 -0300

    Atualização Activity

--- app/src/main/java/com/moisespellegrin/agrocalculo/HomeActivity.kt
+++ app/src/main/java/com/moisespellegrin/agrocalculo/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.moisespellegrin.agrocalculo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,28 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        val btnSementes = findViewById<Button>(R.id.btn_sementes)
+        val btnFertilizantes = findViewById<Button>(R.id.btn_fertilizantes)
+        val btnRegSameadeira = findViewById<Button>(R.id.btn_reg_sameadeira)
+        // Ação do botão sementes
+        btnSementes.setOnClickListener {
+            val intent = Intent(this, SementesActivity::class.java)
+            startActivity(intent)
+        }
+        // Ação do botão fertilizantes
+        btnFertilizantes.setOnClickListener {
+            val intent = Intent(this, FertilizantesActivity::class.java)
+            startActivity(intent)
+        }
+        // Ação do botão regulagem da sameadeira
+        btnRegSameadeira.setOnClickListener {
+            val intent = Intent(this, RegSameadeiraActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/moisespellegrin/agrocalculo/RegSameadeiraActivity.kt
@@ -0,0 +1,20 @@
+package com.moisespellegrin.agrocalculo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegSameadeiraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reg_sameadeira)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
