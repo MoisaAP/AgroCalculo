443187f0fef6227f267367fc5c1af551edd7c44b
MoisaAP <moisespellegrin@gmail.com>
2025-11-03 21:38:08 -0300

    details

--- app/src/main/java/com/moisespellegrin/agrocalculo/HomeActivity.kt
+++ app/src/main/java/com/moisespellegrin/agrocalculo/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.moisespellegrin.agrocalculo
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +11,12 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageButton
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,6 +39,7 @@ class HomeActivity : AppCompatActivity() {
         val btnPMS = findViewById<Button>(R.id.btn_pms)
         val btnRegSameadeira = findViewById<Button>(R.id.btn_reg_sameadeira)
         val btnHistorico = findViewById<Button>(R.id.btn_calc_salvos)
+        val btnFeedback = findViewById<ImageButton>(R.id.btn_feedback)
 
         // Ação do botão sementes
         btnSementes.setOnClickListener {
@@ -58,7 +65,16 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
             overridePendingTransition(0, 0)
         }
+        // Ação do botão feedback
+        val webView = findViewById<WebView>(R.id.webview_feedback)
+        btnFeedback.setOnClickListener {
 
+            // Torna o WebView visível e carrega o link
+            webView.visibility = View.VISIBLE
+            webView.settings.javaScriptEnabled = true
+            webView.webViewClient = WebViewClient()
+            webView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSe-fJdYi7MvK0DVqkvl8t25rpj2W0mPhIUlf1rydd9SJ9Vl1g/viewform?usp=dialog")
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
